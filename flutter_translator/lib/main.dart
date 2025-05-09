571466e860d24b4fcab298d0fda8243217b4a4c9
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2025-05-09 18:17:51 -0300

    puglin

--- flutter_translator/lib/main.dart
+++ flutter_translator/lib/main.dart
@@ -6,46 +6,43 @@ import 'package:image_picker/image_picker.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:translator/translator.dart';
 
-
-
 void main() {
-  runApp(
-    MaterialApp(debugShowCheckedModeBanner: false, home: AppTranslator()),
-  );
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: AppTranslator()));
 }
 
-class AppTranslator extends StatefulWidget {AppTranslator({Key});
+class AppTranslator extends StatefulWidget {
+  AppTranslator({Key});
   @override
   State<AppTranslator> createState() => _AppTranslatorState();
 }
 
-class _AppTranslatorState extends State<AppTranslator> { 
+class _AppTranslatorState extends State<AppTranslator> {
   final TextEditingController _textController = TextEditingController();
-final translator = GoogleTranslator();
-String translatedText = '';
-String _selectedLanguage = 'en'; // por exemplo
+  final translator = GoogleTranslator();
+  String translatedText = '';
+  String _selectedLanguage = 'en'; // por exemplo
 
+  FutureOr<void> translateText() async {
+    final input = _textController.text;
+    if (input.isEmpty) return;
+    final translation = await translator.translate(
+      input,
+      to: _selectedLanguage,
+    );
 
-FutureOr<void> translateText() async {
-  final input = _textController.text;
-  if (input.isEmpty) return;
-  final translation = await translator.translate(input, to: _selectedLanguage);
-  
-
-  setState(() {
-    translatedText = translation.text;
-  });
-}
-
+    setState(() {
+      translatedText = translation.text;
+    });
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("flutter_translator")), 
+      appBar: AppBar(title: Text("flutter_translator")),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Bora"),
+          Text("Tradutor de Texto"),
           Padding(
             padding: EdgeInsets.all(8.0),
             child: TextField(
@@ -55,27 +52,25 @@ FutureOr<void> translateText() async {
                 border: OutlineInputBorder(),
               ),
             ),
-
-
           ),
           ElevatedButton(onPressed: translateText, child: Text("Traduzir")),
-          Text('Tradução:', style: TextStyle(fontWeight: FontWeight.bold)),Text(translatedText),
-            DropdownButton<String>(
-              value: _selectedLanguage,
-              onChanged: (String? value) {
-                setState(() {
-                  _selectedLanguage = value!;
-                });
-              },
-              items: const [
-                DropdownMenuItem(value: 'en', child: Text('Inglês')),
-                DropdownMenuItem(value: 'es', child: Text('Espanhol')),
-                DropdownMenuItem(value: 'fr', child: Text('Francês')),
-              ],
-            ),
+          Text('Tradução:', style: TextStyle(fontWeight: FontWeight.bold)),
+          Text(translatedText),
+          DropdownButton<String>(
+            value: _selectedLanguage,
+            onChanged: (String? value) {
+              setState(() {
+                _selectedLanguage = value!;
+              });
+            },
+            items: const [
+              DropdownMenuItem(value: 'en', child: Text('Inglês')),
+              DropdownMenuItem(value: 'es', child: Text('Espanhol')),
+              DropdownMenuItem(value: 'fr', child: Text('Francês')),
+            ],
+          ),
         ],
       ),
     );
   }
 }
-  
